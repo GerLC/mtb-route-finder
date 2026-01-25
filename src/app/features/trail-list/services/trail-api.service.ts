f56eced68c0ac76cf7cf9af1171aa9c92e2487bc
GerLC <39887800+GerLC@users.noreply.github.com>
2026-01-25 00:43:29 -0400

    refactor(api): add zod parse to trail api

--- src/app/features/trail-list/services/trail-api.service.ts
+++ src/app/features/trail-list/services/trail-api.service.ts
@@ -1,10 +1,12 @@
 import { httpResource } from '@angular/common/http';
 import { Injectable } from '@angular/core';
-import { Trail } from '../../../core/interfaces/trail.interface';
+import { TrailSchema } from '../../../core/interfaces/trail.schema';
 
 @Injectable({
   providedIn: 'root',
 })
 export class TrailApiService {
-  trailsResource = httpResource(() => '/api/trails');
+  trailsResource = httpResource(() => '/api/trails', {
+    parse: TrailSchema.parse,
+  });
 }
